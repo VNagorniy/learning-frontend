acd0eca6003dd312363c8c30e069f45cfb316606
VNagorniy <vnagorniyrnd@gmail.com>
2025-07-25 18:05:49 +0300

    feat/codewars: add task key 6

--- codewars/key-6/index.js
+++ codewars/key-6/index.js
@@ -80,3 +80,9 @@ function alphabetPosition(text) {
 				.join(' ')
 		: '';
 }
+
+//------------------------------
+//Bit Counting https://www.codewars.com/kata/526571aae218b8ee490006f4/train/javascript
+function countBits(n) {
+	return n.toString(2).split('0').join('').length;
+}
