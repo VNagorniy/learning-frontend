a7e9199784aa966f082cda55a42177fa286097c8
VNagorniy <vnagorniyrnd@gmail.com>
2025-08-04 18:43:33 +0300

    feat/vanillaJS/elements/switch-elements: add switch element#1

---
+++ vanilla-js/elements/switch-elements/switch-element1/script.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', () => {
+	const rockerSwitch = new RockerSwitch('#temperature-scale');
+});
+
+class RockerSwitch {
+	/** Button used for this switch */
+	constructor(buttonEl) {
+		this.button = document.querySelector(buttonEl);
+		this.button.addEventListener('click', this.temperatureScaleToggle.bind(this));
+		this._temperatureScale = 'f';
+	}
+	/** Selected temperature scale */
+	get temperatureScale() {
+		return this._temperatureScale;
+	}
+	set temperatureScale(value) {
+		this._temperatureScale = value;
+		this.button.setAttribute('aria-labelledby', this._temperatureScale);
+	}
+	/** Set the temperature scale to °C or °F. */
+	temperatureScaleToggle() {
+		this.temperatureScale = this.temperatureScale === 'c' ? 'f' : 'c';
+	}
+}
