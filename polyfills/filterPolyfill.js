02cfccbd4e49dba518608a79cf8ab6f7edb04666
VNagorniy <vnagorniyrnd@gmail.com>
2025-09-03 21:36:32 +0300

    feat/polyfills: second option for filter func

--- polyfills/filterPolyfill.js
+++ polyfills/filterPolyfill.js
@@ -6,4 +6,15 @@ Array.prototype.myFilter = function (func) {
 	return result;
 };
 
+//2й способ
+Array.prototype.myFilter = function (callback) {
+	let result = [];
+	for (let i = 0; i < this.length; i++) {
+		if (callback(this[i], i, this)) {
+			result.push(this[i]);
+		}
+	}
+	return result;
+};
+
 console.log(digits.myFilter((item) => item % 2));
